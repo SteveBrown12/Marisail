f1862b1c3177662a5d5c3156e0b1091c2afa576c
Karizali <karizali310@gmail.com>
2025-07-30 21:12:00 +0500

    "Done"

--- node-api/src/index.js
+++ node-api/src/index.js
@@ -1,31 +1,36 @@
 import { Router } from "express";
 import homeRouter from "./routes/home.js";
-import advertBerthRouter from "./routes/advert_berth.js";
-import advertEngineRouter from "./routes/advert_engine.js";
-import advertTrailerRouter from "./routes/advert_trailer.js";
+
+import genericAdvertRouter from "./routes/generic_advert.js";
+import genericSearchRouter from "./routes/generic_search.js"
+
 import searchEngineRouter from "./routes/search_engine.js";
 import searchTrailerRouter from "./routes/search_trailer.js";
 import searchBerthRouter from "./routes/search_berth.js";
 import searchShopRouter from "./routes/search_Shop.js";
 import searchCharterRouter from "./routes/search_charter.js";
 import searchTransportRouter from "./routes/search_transport.js";
-import advertCharterRouter from "./routes/advert_charter.js";
-import advertTransportRouter from "./routes/advert_transport.js";
-import advertChandleryRouter from "./routes/advert_shop.js";
-import advertAuctionRouter from "./routes/advert_auction.js";
 
 const router = Router();
 
 // Use the homeRouter for requests to /api/home
 router.use("/home", homeRouter);
 
-router.use("/advert_berth", advertBerthRouter);
+router.use("/advert_berth", genericAdvertRouter);
 
-router.use("/advert_engine", advertEngineRouter);
+router.use("/advert_engine", genericAdvertRouter);
 
-router.use("/trailers", advertTrailerRouter);
+router.use("/trailers", genericAdvertRouter);
 
-router.use("/search_engine", searchEngineRouter);
+router.use("/advert_charter", genericAdvertRouter);
+
+router.use("/advert_transport", genericAdvertRouter);
+
+router.use("/advert_chandlery", genericAdvertRouter);
+
+router.use("/advert_auction", genericAdvertRouter);
+
+router.use("/search_engine", genericSearchRouter);
 
 router.use("/search_trailer", searchTrailerRouter);
 
@@ -36,14 +41,6 @@ router.use("/search_Shop", searchShopRouter);
 router.use("/search_charter", searchCharterRouter);
 
 router.use("/search_transport", searchTransportRouter);
-
-router.use("/advert_charter", advertCharterRouter);
-
-router.use("/advert_transport", advertTransportRouter);
-
-router.use("/advert_chandlery", advertChandleryRouter);
-
-router.use("/advert_auction", advertAuctionRouter);
 
 // Export the router
 export default router;
