32c41cfb9296e28f3a20ae32bdde6d5ab6ea7bf9
md-shahid-ansari <sajarsadaf2000@gmail.com>
2025-08-05 11:15:12 +0530

    node-api updated (modified by atees) and PORT from dotenv added in server.js

--- node-api/src/index.js
+++ node-api/src/index.js
@@ -1,16 +1,15 @@
+
+
 import { Router } from "express";
 import homeRouter from "./routes/home.js";
-import advertBerthRouter from "./routes/Berth_Advert.js";
-import advertEngineRouter from "./routes/Engine_Advert.js";
-import advertTrailerRouter from "./routes/Trailer_Advert.js";
-import searchEngineRouter from "./routes/Engine_Search.js";
-import searchTrailerRouter from "./routes/Trailer_Search.js";
-import searchBerthRouter from "./routes/Berth_Search.js";
-import searchCharterRouter from "./routes/Charter_Search.js";
-import searchTransportRouter from "./routes/Transport_Search.js";
-import advertCharterRouter from "./routes/Charter_Advert.js";
-import advertTransportRouter from "./routes/Transport_Advert.js";
+
 import handler from "./routes/upload-media.js";
+
+
+
+import search_router from "./routes/Generic_search.js"; 
+import advert_router from "./routes/Generic_Advert.js"; 
+
 import authRouter from "./routes/auth.js";
 
 const router = Router();
@@ -21,28 +20,11 @@ router.use("/home", homeRouter);
 // auth router
 router.use("/auth", authRouter);
 
-router.use("/advert_berth", advertBerthRouter);
 
-router.use("/advert_engine", advertEngineRouter);
-
-router.use("/trailers", advertTrailerRouter);
-
-router.use("/search_engine", searchEngineRouter);
-
-router.use("/search_trailer", searchTrailerRouter);
-
-router.use("/search_berth", searchBerthRouter);
-
-router.use("/search_charter", searchCharterRouter);
-
-router.use("/search_transport", searchTransportRouter);
-
-router.use("/advert_charter", advertCharterRouter);
-
-router.use("/advert_transport", advertTransportRouter);
+router.use("/", search_router); // This is the new search router
+router.use("/", advert_router); // This is the new search router
 
 
-router.use("/upload-media", handler); // upload-media upload
+router.use("/upload-media", handler);
 
-// Export the router
 export default router;
